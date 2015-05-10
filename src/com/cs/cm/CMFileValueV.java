4ffec8fd2157a001487ba1d3215f99cac097ef9e
xiaoheier <xiaoheier@126.com>
2015-05-10 21:14:37 +0800

    add struct for CM
    add struct for CM

---
+++ src/com/cs/cm/CMFileValueV.java
@@ -0,0 +1,18 @@
+package com.cs.cm;
+
+public class CMFileValueV {
+	private int index;
+	private String value;
+	public int getIndex() {
+		return index;
+	}
+	public void setIndex(int index) {
+		this.index = index;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
